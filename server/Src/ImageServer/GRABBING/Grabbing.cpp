8f315e95a6af8f1cca4eed6b2e92008164807cc5
De Paulis Manuel <depaulis.manuel.s@gmail.com>
2022-06-10 14:04:55 +0200

    Started implement architecture

---
+++ server/Src/ImageServer/GRABBING/Grabbing.cpp
@@ -0,0 +1,9 @@
+#include "Grabbing.h"
+
+Grabbing::Grabbing()
+{
+}
+
+Grabbing::~Grabbing()
+{
+}
--- server/Src/ImageServer/Main.cpp
+++ server/Src/ImageServer/Main.cpp
@@ -1,4 +1,6 @@
-
+#include "GRABBING/Grabbing.h"
+#include "PROCESSING/Processing.h"
+#include "GUI/GUICommunication.h"
 
 #include <opencv2/opencv.hpp>
 #include <boost/version.hpp>
@@ -13,7 +15,7 @@ int main()
     std::cout << "USING NLOHMANN JSON VERSION: " << NLOHMANN_JSON_VERSION_MAJOR << "." << NLOHMANN_JSON_VERSION_MINOR << "." << NLOHMANN_JSON_VERSION_PATCH << "\n";
     std::cout << "USING WEBSOCKET VERSION: " << websocketpp::user_agent << "\n";
 
-    std::cout << "Hello World!\n";
+    return 0;
 }
 
 
