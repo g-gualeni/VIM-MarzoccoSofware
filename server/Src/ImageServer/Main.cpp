6fb2d2deff77fa33ff6432d625ce6bcbea4d346e
De Paulis Manuel <depaulis.manuel.s@gmail.com>
2022-06-10 12:06:18 +0200

    Backup string

--- server/Src/ImageServer/Main.cpp
+++ server/Src/ImageServer/Main.cpp
@@ -11,7 +11,7 @@ int main()
     std::cout << "USING CV VERSION: " << cv::getVersionString() << "\n";
     std::cout << "USING BOOST VERSION: " << BOOST_LIB_VERSION << "\n";
     std::cout << "USING NLOHMANN JSON VERSION: " << NLOHMANN_JSON_VERSION_MAJOR << "." << NLOHMANN_JSON_VERSION_MINOR << "." << NLOHMANN_JSON_VERSION_PATCH << "\n";
-    std::cout << "USING WEBSOCKET VERSION " << websocketpp::user_agent << "\n";
+    std::cout << "USING WEBSOCKET VERSION: " << websocketpp::user_agent << "\n";
 
     std::cout << "Hello World!\n";
 }
